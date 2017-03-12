73d0ed7a2435ba669696f76c8b6adae8c983cca1
Estime Ntwari <estimentwari@Estimes-MacBook-Pro.local>
2017-03-12 17:09:09 -0400

    Added responsive UI

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,21 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props)=>{
   return (
-      <h3>Examples</h3>,
-      <p>Welcome to the example page</p>
-    )
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Hamilton'>Hamilton, On</Link>
+        </li>
+        <li>
+          <Link to='/?location=Tokyo'>Tokyo, Jp</Link>
+        </li>
+      </ol>      
+    </div>
+      
+    );
 };
 module.exports = Examples;
