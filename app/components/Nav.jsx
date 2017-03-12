3dabfd386130dc7442a1c029a73ea68680b3acd8
Estime Ntwari <estimentwari@Estimes-MacBook-Pro.local>
2017-03-12 16:35:19 -0400

    Added navigation bar styling

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,15 +1,48 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = (props)=>{
+var Nav = React.createClass({
+  onSearch: function (e){
+    e.preventDefault();
+    alert('Not yet wired up!')
+  },
+  render: function(){
     return (
-      <div>
-        <h2>Estime's Web Application - Testing</h2>
-        <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-        <Link to="/about" activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>About</Link>
-        <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+      <div className= "top-bar">
+        <div className = "top-bar-left">
+          <ul className="menu">
+            <li className = "menu-text">
+              Estime's Demo WeatherApp
+            </li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>About</Link>
+            </li>
+            <li>  
+              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className = "top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className ="menu">
+              <li>
+                <input type="search" placeholder="Search City"/>
+              </li>
+              <li>
+                <input type="submit" className = "button" value="Get Weather"/>
+              </li>
+            </ul>
+          </form>
+        </div>
       </div>
     );
-}
+  }
+});
+    
+
 
 module.exports = Nav;
+
